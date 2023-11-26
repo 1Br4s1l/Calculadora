7ef5d042ad075938b01851ee3349570cc0af3fc3
1Br4s1l <matheuseenio@gmail.com>
2023-11-26 18:47:22 -0300

    Foi adicionada um canvas.

--- script.js
+++ script.js
@@ -87,4 +87,30 @@ function init(){
   })
 }
 
+ var canvas = document.getElementById("myCanvas");
+ var context = canvas.getContext("2d");
+ var isDrawing = false;
+
+ canvas.addEventListener("mousedown", startDrawing);
+ canvas.addEventListener("mousemove", draw);
+ canvas.addEventListener("mouseup", stopDrawing);
+ canvas.addEventListener("mouseout", stopDrawing);
+ function startDrawing(e) {
+     isDrawing = true;
+     draw(e); 
+ }
+ function draw(e) {
+     if (!isDrawing) return;
+     context.lineWidth = 2;
+     context.lineCap = "round";
+     context.strokeStyle = "#000";
+     context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+     context.stroke();
+     context.beginPath();
+     context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+ }
+ function stopDrawing() {
+     isDrawing = false;
+     context.beginPath();
+ }
 init();
